e8a983e51de346ddeeeac282e90d3b099b0501fc
sambouzar <sambouzar@gmail.com>
2016-02-26 15:01:15 +0100

    Set dropdown menu

--- client/app/components/Nav/NavComponent.ts
+++ client/app/components/Nav/NavComponent.ts
@@ -1,23 +1,30 @@
 import {Component, OnInit} from 'angular2/core';
 import {Router, RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router'
-import {Collapse} from 'ng2-bootstrap/ng2-bootstrap';
+import {DROPDOWN_DIRECTIVES} from 'ng2-bootstrap/ng2-bootstrap';
 
 @Component({
   selector: 'muser-menu',
   templateUrl: 'app/components/views/NavComponent.html',
-  directives: [ROUTER_DIRECTIVES, Collapse]
+  directives: [ROUTER_DIRECTIVES, DROPDOWN_DIRECTIVES]
 })
 
 export class NavComponent implements OnInit {
 
-	private isCollapsed : boolean
+	private disabled:boolean = false;
+  private status:{isopen:boolean} = {isopen: false};
+  private items:Array<string> = ['The first choice!', 'And another choice for you.', 'but wait! A third!'];
 
-	constructor() { this.isCollapsed = false; }
+	constructor() { }
+	
+	private toggled(open:boolean):void {
+	  console.log('Dropdown is now: ', open);
+  }
 
-  toggle() {
-		this.isCollapsed = !this.isCollapsed;
+	private toggleDropdown($event:MouseEvent):void {
+    $event.preventDefault();
+    $event.stopPropagation();
+    this.status.isopen = !this.status.isopen;
   }
 
   ngOnInit() { }
 }
-
